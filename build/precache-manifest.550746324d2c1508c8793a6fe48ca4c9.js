9b35a2e17ccb9d8a32accf635c4338ff50c9e334
Lephilosophe237 <jngan077@uottawa.ca>
2023-07-02 11:38:17 -0400

    first comit

---
+++ build/precache-manifest.550746324d2c1508c8793a6fe48ca4c9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc1df9f4fdefd29a9e4d7bcc652a4a2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f8a8c8222638aef80a6",
+    "url": "/static/css/2.13c9dd37.chunk.css"
+  },
+  {
+    "revision": "d07b2d9485ff642bf1c3",
+    "url": "/static/css/main.a973efef.chunk.css"
+  },
+  {
+    "revision": "3f8a8c8222638aef80a6",
+    "url": "/static/js/2.6b4e19c6.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.6b4e19c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d07b2d9485ff642bf1c3",
+    "url": "/static/js/main.b82e3ddf.chunk.js"
+  },
+  {
+    "revision": "97c002b8f8a25334b102",
+    "url": "/static/js/runtime-main.67d84520.js"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  }
+]);
